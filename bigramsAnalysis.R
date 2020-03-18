a83a836e1077c12e8c72add25012310d6761015c
Paolo Coraggio <pacoraggio@gmail.com>
2020-03-18 10:28:39 +0000

    bigrams analysis - new dataset

--- bigramsAnalysis.R
+++ bigramsAnalysis.R
@@ -23,10 +23,10 @@ source("cleanCorpus.R")
 source("wordfrequency.R")
 source("tidyNgrams.R")
 
-load("news_2percent.RData")
-load("blogs_2percent.RData")
-load("twitter_2percent.RData")
-load("complete_2percent.RData")
+load("news_2percentNoProf.RData")
+load("blogs_2percentNoProf.RData")
+load("twitter_2percentNoProf.RData")
+load("complete_2PercentNoProf.RData")
 
 tic()
 news.bigrams <- create.bigramsfreq(df.news)
@@ -34,8 +34,11 @@ blogs.bigrams <- create.bigramsfreq(df.blogs)
 twitter.bigrams <- create.bigramsfreq(df.twitter)
 complete.bigrams <- create.bigramsfreq(df.complete)
 toc()
-
-# 18.55 sec elapsed
+# 18.55 sec elapsed - 21.31 sec elapsed
+save(news.bigrams, file = "news_bigrams.RData")
+save(blogs.bigrams, file = "blogs_bigrams.RData")
+save(twitter.bigrams, file = "twitter_bigrams.RData")
+save(complete.bigrams, file = "complete_bigrams.RData")
 
 tic()
 news.bigramsSW <- create.bigramsfreq(df.news, remove.stopwords = TRUE)
@@ -44,19 +47,55 @@ twitter.bigramsSW <- create.bigramsfreq(df.twitter, remove.stopwords = TRUE)
 complete.bigramsSW <- create.bigramsfreq(df.complete, remove.stopwords = TRUE)
 toc()
 
-# 17.39 sec elapsed
+save(news.bigramsSW, file = "news_bigramsSW.RData")
+save(blogs.bigramsSW, file = "blogs_bigramsSW.RData")
+save(twitter.bigramsSW, file = "twitter_bigramsSW.RData")
+save(complete.bigramsSW, file = "complete_bigramsSW.RData")
 
-pnews <- plotbar.ngramf(news.bigrams, title = "News Bigrams Frequency", topn = 15, gram = 2)
-pblogs <- plotbar.ngramf(blogs.bigrams, title = "Blogs Bigrams Frequency", topn = 15, gram = 2)
-ptwitter <- plotbar.ngramf(twitter.bigrams, title = "Twitter Bigrams Frequency", topn = 15, gram = 2)
-pcomplete <- plotbar.ngramf(complete.bigrams, title = "Complete Bigrams Frequency", topn = 15, gram = 2)
+# 17.39 sec elapsed - 15.53 sec elapsed
 
-pnewsSW <- plotbar.ngramf(news.bigramsSW, title = "News Bigrams Frequency", topn = 15, gram = 2)
-pblogsSW <- plotbar.ngramf(blogs.bigramsSW, title = "Blogs Bigrams Frequency", topn = 15, gram = 2)
-ptwitterSW <- plotbar.ngramf(twitter.bigramsSW, title = "Twitter Bigrams Frequency", topn = 15, gram = 2)
-pcompleteSW <- plotbar.ngramf(complete.bigramsSW, title = "Complete Bigrams Frequency", topn = 15, gram = 2)
+pbigramsnews <- plotbar.ngramf(news.bigrams, title = "News Bigrams Frequency", topn = 15, gram = 2)
+pbigramsblogs <- plotbar.ngramf(blogs.bigrams, title = "Blogs Bigrams Frequency", topn = 15, gram = 2)
+pbigramstwitter <- plotbar.ngramf(twitter.bigrams, title = "Twitter Bigrams Frequency", topn = 15, gram = 2)
+pbigramscomplete <- plotbar.ngramf(complete.bigrams, title = "Complete Bigrams Frequency", topn = 15, gram = 2)
+
+pbigramsnewsSW <- plotbar.ngramf(news.bigramsSW, title = "News Bigrams Frequency", topn = 15, gram = 2)
+pbigramsblogsSW <- plotbar.ngramf(blogs.bigramsSW, title = "Blogs Bigrams Frequency", topn = 15, gram = 2)
+pbigramstwitterSW <- plotbar.ngramf(twitter.bigramsSW, title = "Twitter Bigrams Frequency", topn = 15, gram = 2)
+pbigramscompleteSW <- plotbar.ngramf(complete.bigramsSW, title = "Complete Bigrams Frequency", topn = 15, gram = 2)
+
+save(pbigramsnews, file = "pbigramsnews.RData")
+save(pbigramsblogs, file = "pbigramsblogs.RData")
+save(pbigramstwitter, file = "pbigramstwitter.RData")
+save(pbigramscomplete, file = "pbigramscomplete.RData")
+
+save(pbigramsnewsSW, file = "pbigramsnewsSW.RData")
+save(pbigramsblogsSW, file = "pbigramsblogsSW.RData")
+save(pbigramstwitterSW, file = "pbigramstwitterSW.RData")
+save(pbigramscompleteSW, file = "pbigramscompleteSW.RData")
 
 windows()
-pnews + pnewsSW
+pbigramsnews + pbigramsnewsSW
 windows()
-pcomplete + pcompleteSW
+pbigramsblogs + pbigramsblogsSW
+
+windows()
+pbigramstwitter + pbigramstwitterSW
+
+windows()
+pbigramscomplete + pbigramscompleteSW
+
+####
+
+load("news_bigrams.RData")
+load("news_bigramsSW.RData")
+load("blogs_bigrams.RData")
+load("blogs_bigramsSW.RData")
+load("twitter_bigrams.RData")
+load("twitter_bigramsSW.RData")
+load("complete_bigrams.RData")
+load("complete_bigramsSW.RData")
+
+tail(complete.bigrams[complete.bigrams$n > 6,])
+tail(twitter.bigrams[twitter.bigrams$n > 2,])
+tail(twitter.bigramsSW[twitter.bigramsSW$n > 2,])
