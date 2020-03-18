764ccb7888b2e3d0146205199db6342a2edd0472
Paolo Coraggio <pacoraggio@gmail.com>
2020-03-18 10:30:41 +0000

    head and tail analysis with no profanity

--- trigramsAnalysis.R
+++ trigramsAnalysis.R
@@ -23,15 +23,10 @@ source("cleanCorpus.R")
 source("wordfrequency.R")
 source("tidyNgrams.R")
 
-# load("news_2percent.RData")
-# load("blogs_2percent.RData")
-# load("twitter_2percent.RData")
-# load("complete_2percent.RData")
-
-load("dfnewsNP.RData")
-load("dfblogsNP.RData")
-load("dftwitterNP.RData")
-load("dfcompleteNP.RData")
+load("news_2percentNoProf.RData")
+load("blogs_2percentNoProf.RData")
+load("twitter_2percentNoProf.RData")
+load("complete_2PercentNoProf.RData")
 
 tic()
 news.trigrams <- create.trigramsfreq(df.news)
@@ -39,8 +34,7 @@ blogs.trigrams <- create.trigramsfreq(df.blogs)
 twitter.trigrams <- create.trigramsfreq(df.twitter)
 complete.trigrams <- create.trigramsfreq(df.complete)
 toc()
-# without restarting R 36.1 sec elapsed
-# 70.91 sec elapsed
+# 36.1 sec elapsed - 33.13 sec elapsed - 36.17 sec elapsed
 
 tic()
 news.trigramsSW <- create.trigramsfreq(df.news, remove.stopwords = TRUE)
@@ -48,7 +42,17 @@ blogs.trigramsSW <- create.trigramsfreq(df.blogs, remove.stopwords = TRUE)
 twitter.trigramsSW <- create.trigramsfreq(df.twitter, remove.stopwords = TRUE)
 complete.trigramsSW <- create.trigramsfreq(df.complete, remove.stopwords = TRUE)
 toc()
-# without restarting R 43.14 sec elapsed
+# 43.14 sec elapsed - 18.86 sec elapsed - 19.2 sec elapsed
+
+save(news.trigrams, file = "news_trigrams.RData")
+save(blogs.trigrams, file = "blogs_trigrams.RData")
+save(twitter.trigrams, file = "twitter_trigrams.RData")
+save(complete.trigrams, file = "complete_trigrams.RData")
+
+save(news.trigramsSW, file = "news_trigramsSW.RData")
+save(blogs.trigramsSW, file = "blogs_trigramsSW.RData")
+save(twitter.trigramsSW, file = "twitter_trigramsSW.RData")
+save(complete.trigramsSW, file = "complete_trigramsSW.RData")
 
 pnews.trigrams <- plotbar.ngramf(news.trigrams, title = "News Trigrams Frequency", topn = 15, gram = 3)
 pblogs.trigrams <- plotbar.ngramf(blogs.trigrams, title = "Blogs Trigrams Frequency", topn = 15, gram = 3)
@@ -60,6 +64,15 @@ pblogs.trigramsSW <- plotbar.ngramf(blogs.trigramsSW, title = "Blogs Trigrams Fr
 ptwitter.trigramsSW <- plotbar.ngramf(twitter.trigramsSW, title = "Twitter Trigrams Frequency", topn = 15, gram = 3)
 pcomplete.trigramsSW <- plotbar.ngramf(complete.trigramsSW, title = "Complete Trigrams Frequency", topn = 15, gram = 3)
 
+save(pnews.trigrams, file = "pnews_trigrams.RData")
+save(pnews.trigramsSW, file = "pnews_trigramsSW.RData")
+save(pblogs.trigrams, file = "pblogs_trigrams.RData")
+save(pblogs.trigramsSW, file = "pblogs_trigramsSW.RData")
+save(ptwitter.trigrams, file = "ptwitter_trigrams.RData")
+save(ptwitter.trigramsSW, file = "ptwitter_trigramsSW.RData")
+save(pcomplete.trigrams, file = "pcomplete.trigrams.RData")
+save(pcomplete.trigramsSW, file = "pcomplete.trigramsSW.RData")
+
 windows()
 pnews.trigrams + pnews.trigramsSW
 windows()
@@ -68,3 +81,14 @@ windows()
 ptwitter.trigrams + ptwitter.trigramsSW
 windows()
 pcomplete.trigrams + pcomplete.trigramsSW
+
+### head and tail
+names(complete.trigrams)
+length(unique(complete.trigrams$trigram))
+nrow(complete.trigrams[complete.trigrams$n > 5,])
+tail(complete.trigrams[complete.trigrams$n > 5,])
+
+nrow(complete.trigrams[complete.trigrams$n > 5,])/nrow(complete.trigrams)
+windows()
+ggplot(complete.trigrams[complete.trigrams$n > 5,], aes(n)) +
+    geom_histogram(bins = 400)
